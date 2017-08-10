41b1c76bc9dfd0a6744572f922a1b5a90ee84f8c
gabyandrade04 <gabrielandrade04@gmail.com>
2017-08-10 15:39:21 -0300

    atributos y objetos en basea atributo

--- src/Publico/Persona.java
+++ src/Publico/Persona.java
@@ -9,6 +9,27 @@ package Publico;
  *
  * @author Español
  */
+import javax.swing.JOptionPane;
+
 public class Persona {
+    
+    int altura =10;
+    public int largo=30;
+    final int ojos=1;
+   public static void main (String [] args){
+       Persona juan = new Persona();
+       System.out.println (juan.altura);
+       juan.altura=20;
+       System.out.println (juan.altura);
+       System.out.println (juan.largo);
+       
+       System.out.println (juan.ojos);
+       Persona pedro= new Persona();
+       System.out.println ("Juan tiene de altura "+juan.altura+", de largo tiene "+juan.largo+" y tiene "+juan.ojos);
+       System.out.println ("Pedro tiene de altura "+pedro.altura+", de largo tiene "+pedro.largo+" y tiene "+pedro.ojos);
+       JOptionPane.showMessageDialog(null, "Juan\n"+ "Tiene de altura:"+juan.altura+"\nTiene de largo:"+juan.largo+"\nTiene "+juan.ojos+" ojo");
+       JOptionPane.showMessageDialog(null, "Pedro\n"+ "Tiene de altura:"+pedro.altura+"\nTiene de largo:"+pedro.largo+"\nTiene "+pedro.ojos+" ojo");
+   }   
+    
     
 }
